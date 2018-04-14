ad5f304b45362e9b91d822d5b3d4e32285b8accd
Valerii Vlasenko <vvlasenk@e3r7p4.unit.ua>
2018-04-14 17:25:21 +0300

    memento mori

---
+++ cmake-build-debug/CMakeFiles/SCOP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SCOP.dir/src/main.c.o"
+  "SCOP.pdb"
+  "SCOP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SCOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
